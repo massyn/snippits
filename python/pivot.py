7135608e1177d60b08ab4e1012cbab3c670703e1
Phil Massyn <massyn@users.noreply.github.com>
2022-04-21 15:56:28 +1000

    Update pivot.py

--- python/pivot.py
+++ python/pivot.py
@@ -2,8 +2,6 @@ class pivot:
     
     def __init__(self,**KW):
         self.data = { 'matrix' : {} , 'x' : [], 'y' : [] }
-        self.stats = {}
-        self.pvt = { 'x' : {}, 'y' : {}}
 
     def plot(self,x,y,z,c = 'neutral'):
         if not x in self.data['x']:
@@ -49,7 +47,51 @@ class pivot:
 if __name__ == '__main__':
     
     with open('c:/temp/test.html','wt') as h:
-        h.write('<html>')
+        h.write('''
+        <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Status report</title>
+                    <style>
+                        body {
+                            font-family: Arial, Helvetica, sans-serif;
+                        }
+                        table {
+                            border-collapse: collapse;
+                            border-spacing: 1px;
+                            border: 1px solid #c0c0c0;
+                        }
+                        th, td {
+                            padding: 5px;
+                            text-align: left;
+                            border: 1px solid #c0c0c0;
+                            text-align: center; 
+                            vertical-align: middle;
+                        }
+                        th {
+                            background-color: #44546A;
+                            color: white;
+                        }
+                        .ok {
+                            background-color: #00B050;
+                            color: white;
+                        }
+                        .error {
+                            background-color: #C00000;
+                            color: white;
+                        }
+                        .warning {
+                            background-color: #FFC000;
+                            color: black;
+                        }
+                        .neutral {
+                            background-color: #E0E0E0;
+                            color: black;
+                        }
+                    </style>
+                </head>
+            <body>
+            ''')
 
         h.write('<h1>Basic matrix usage</h1>')
 
@@ -57,3 +99,4 @@ if __name__ == '__main__':
         pvt.plot('Australia','Wallaby','Yes','red')
         pvt.plot('South Africa','Springbok','Yes')
         h.write(pvt.display())
+        h.write('</body></html>')
